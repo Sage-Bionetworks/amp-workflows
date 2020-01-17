6766cd896ffe2add0e92e6602baa7d83cff9d852
wpoehlm <william.poehlman@sagebase.org>
2020-01-17 21:07:18 +0000

    added script that converts synids to specimenID and removes duplicates from output tables

--- amp-rnaseq_reprocessing/amp-rnaseq_reprocess-workflow/utils/clean_tables.py
+++ amp-rnaseq_reprocessing/amp-rnaseq_reprocess-workflow/utils/clean_tables.py
@@ -6,8 +6,9 @@ import shutil
 
 """Convert Synids
 
-This script converts synapse ids into specimen ids for output files 
-from the AMP-AD RNASeq reprocessing workflow.
+This script converts synapse ids into specimen ids for output files
+from the AMP-AD RNASeq reprocessing workflow.  The script also removes
+duplicate samples from the tables and write a set of "clean" tables
 """
 
 def parse_samples(prov):
@@ -25,7 +26,7 @@ def parse_samples(prov):
 
 def update_count_header(sampledict, counttable):
     """Convert synapseid to specimen ID in header of count table"""
-    from_file = open(counttable)
+    from_file = open(counttable) 
     line = from_file.readline().strip()
 
     # convert the synapsee IDs to specimen IDs
@@ -39,8 +40,13 @@ def update_count_header(sampledict, counttable):
     # replace original header with converted header
     sep = '\t'
     to_file = open('gene_counts_specimenid.txt',mode='w')
-    to_file.write(sep.join(converted))
+    to_file.write(sep.join(converted) + '\n')
     shutil.copyfileobj(from_file, to_file)
+
+    # Remove duplicate samples
+    df = pd.read_csv('gene_counts_specimenid.txt', sep='\t', index_col='feature')
+    df_clean = df.astype(int).fillna(0).T.drop_duplicates().T
+    df_clean.to_csv(path_or_buf='gene_counts_clean.txt',sep='\t')
 
 def update_log_header(sampledict, logfile):
     """convert synapseid to specimenid in first column of star log table"""
@@ -57,6 +63,12 @@ def update_log_header(sampledict, logfile):
             sep = '\t'
             print(str(specimen + sep) + sep.join(content), file=open('star_log_specimenid.txt', 'a'))
 
+    # remove duplicate samples
+    df = pd.read_csv('star_log_specimenid.txt', sep='\t', index_col='Sample')
+    # Since this table contains computational runtimes and mapping speeds, use only a subset of fields to idenify duplicates
+    df_clean = df.drop_duplicates(subset=['Number of input reads', 'Uniquely mapped reads number'])
+    df_clean.to_csv(path_or_buf='star_log_clean.txt',sep='\t')
+
 def update_metrics_header(sampledict, metrics):
     """convert synapseid to specimenid in headerof picard metrics file"""
     count = 0
@@ -72,6 +84,11 @@ def update_metrics_header(sampledict, metrics):
             sep = '\t'
             print(str(specimen + sep) + sep.join(content), file=open('sample_metrics_specimenid.txt', 'a'))
 
+    # Remove duplicate samples
+    df = pd.read_csv('sample_metrics_specimenid.txt', sep='\t', index_col='sample')
+    df_clean = df.drop_duplicates()
+    df_clean.to_csv(path_or_buf='sample_metrics_clean.txt',sep='\t')
+
 def main():
     # create synapseid:sampleid dictionary
     sampledict = parse_samples('provenance.csv')
@@ -82,3 +99,8 @@ def main():
     update_metrics_header(sampledict, 'Study_all_metrics_matrix.txt')
 
 main()
+
+
+
+
+
